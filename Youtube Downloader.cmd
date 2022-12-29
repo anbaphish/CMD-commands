67f76b48c7885b7f86838060fec7aa0f9f92519f
Phish <78632176+anbaphish@users.noreply.github.com>
2022-12-29 00:15:43 -0800

    YT-DL quick downloader
    Made so I don't have to CD to YT-DL and run it and enter in specific commands to download only MP3 and such. Cd's to D:\youtube dl and runs youtube-dl in there

---
+++ Youtube Downloader.cmd
@@ -0,0 +1,18 @@
+@echo off
+echo Input youtube link: 
+set /p input= Enter: 
+cd /D "D:youtube dl"
+echo Do you want this was an mp3 (y or n): 
+set /p bool= Enter: 
+IF %bool%==y (youtube-dl -x --audio-format mp3 %input%  && exit) ELSE (echo "entering into ytdl selection mode")
+
+
+youtube-dl -F %input% 
+
+echo input format (Audio + video): 
+
+set /p format= Enter: 
+
+youtube-dl -f %format% %input%
+
+set /p format= Enter:
